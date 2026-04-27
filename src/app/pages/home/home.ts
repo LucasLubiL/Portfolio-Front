702b5446ceca1b0646b0021c89bc8a360e92deab
LucasLubiL <lucasubiii@gmail.com>
2026-04-27 16:50:30 -0300

    Projeto finalizado para deploy

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from '../../shared/header/header';
+import { Footer } from '../../shared/footer/footer';
 
 @Component({
   selector: 'app-home',
-  imports: [Header],
+  imports: [Header, Footer],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
